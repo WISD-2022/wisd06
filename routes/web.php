09c6bb66837f0c3f3343048ab4443eca72c3c3d0
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2022-11-30 11:55:28 +0800

    aa.a1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+//Auth::routes();
+
+Route::get('/home','HomeController@index')->name('home');
+
+Route::get('/aa/al',function (){
+    return view('aa.a1');
 });
 
 Route::middleware([
